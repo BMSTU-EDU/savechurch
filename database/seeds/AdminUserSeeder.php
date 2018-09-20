416524e67e62ea19308f9e06c50bca705e6a8530
Dmitry Kozin <kds@EQUIFAX.ru>
2018-09-20 14:04:16 +0300

    Create admin seeder, protect registration, create Church model, controller, migration

---
+++ database/seeds/AdminUserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+			'name'		=> env('ADMIN_LOGIN'),
+			'email'		=> env('ADMIN_EMAIL'),
+			'password'	=> Hash::make(env('ADMIN_PASSWORD'))
+		]);
+    }
+}
